f21f83de41ffe39478f5fd86e739650dc6b23948
sawa <d.2ysactbec.sthz23@gmail.com>
2021-03-02 22:49:44 +0900

    初回コミット

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    let timer = 0;
+    let milliSeconds = 0;
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+
+    $("milliSeconds").html(milliSeconds);
+    $("seconds").html(seconds);
+    $("minutes").html(minutes);
+    $("hours").html(hours);
+
+    $()
+})
